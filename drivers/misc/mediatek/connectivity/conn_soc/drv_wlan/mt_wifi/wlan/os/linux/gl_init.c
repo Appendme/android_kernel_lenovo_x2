cdfcbc0855401c0bdd1e69460ee69b308a72bce5
dragonpt <cesar.maximo@gmail.com>
2015-12-26 22:53:22 +0000

    wlan: WiFi Direct CTS fixs Cylen Yao <cylen.yao@mediatek.com>
    Details:
      1. WiFi Direct CTS tests will fail as supplicant and driver could
         not keep sync in following case:
    
         1.1 supplicant will request channel when do p2p listen, but
             driver/firmware has not switch to the target channel when
             supplicant get remain on channel credit by call driver API
             of remain on channel; This will make supplicant and driver
             in unsync state which will make supplicant fail to go to
             listen state randomly.
    
         1.2 Supplicant and driver will also keep unsync when do mgmt
             frame TX; supplicant will do other task once mgmt frame TX
             is returned by calling  driver API mgmt_tx, but, driver has
             not actually TX the mgmt frame out. In extremely case, driver
             will drop the second mgmt frame if the previous on has not
             been TX out, just as the group owner test case.

--- drivers/misc/mediatek/connectivity/conn_soc/drv_wlan/mt_wifi/wlan/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/conn_soc/drv_wlan/mt_wifi/wlan/os/linux/gl_init.c
@@ -2362,6 +2362,8 @@ wlanNetCreate(
     init_completion(&prGlueInfo->rScanComp);
     init_completion(&prGlueInfo->rHaltComp);
     init_completion(&prGlueInfo->rPendComp);
+    init_completion(&prGlueInfo->rChannelReq);
+
 #if CFG_ENABLE_WIFI_DIRECT
     init_completion(&prGlueInfo->rSubModComp);
 #endif
